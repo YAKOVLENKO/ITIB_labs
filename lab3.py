0101c6fead64d01cebf9752d6168eb53aaf4094d
YAKOVLENKO <svobodinayakovlenko@yandex.ru>
2019-04-30 14:41:16 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# Исследование нейронных сетей с радиальными базисными функциями (RBF) на примере моделирования булевых выражений
 
 import numpy as np
 from itertools import product, combinations
